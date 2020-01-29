ea26f51d67dd60b5225ffa3a253de0bf745325bc
josh-aki <stg.an.cou.19@gmail.com>
2020-01-29 20:09:01 +0900

    delete console

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -62,7 +62,6 @@ $(function(){
   })
   var reloadMessages = function() {
     last_message_id = $('.chat-main__message-list__message:last').data("message-id");
-    console.log(last_message_id);
     $.ajax({
       url: "api/messages",
       type: 'GET',
